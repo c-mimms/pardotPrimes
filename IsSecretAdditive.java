ab88048fe2f8d13295646305c5e8f5927411af82
Chris Mimms <Chris Mimms>
2016-05-02 23:08:59 -0400

    Added faster prime generator and replaced iterator with a for loop.

--- IsSecretAdditive.java
+++ IsSecretAdditive.java
@@ -24,29 +24,26 @@ public class IsSecretAdditive {
 			System.exit(1);
 		}
 
-		List<Integer> primes = getPrimes(inputNumber);
+		List<Integer> primes = getPrimes2(inputNumber);
 
 		// TODO
 		// create a map or array to store returned values of secret?
 
-		Iterator<Integer> primeIterator = primes.iterator();
-		while (primeIterator.hasNext()) {
-			int prime = primeIterator.next();
+		int numPrimes = primes.size();
+		for (int i = 0; i < numPrimes; i++) {
+
 			// Save this so we don't call secret unnecessarily
-			int secret1 = secret(prime);
-			// First prime was removed from iterator and needs to be checked
-			// against itself.
-			if (secret1 * 2 != secret(prime + prime)) {
-				System.out.println("Secret is not additive!");
-				System.exit(0);
-			}
-			primeIterator.forEachRemaining(prime2 -> {
-				if (secret1 + secret(prime2) != secret1 + secret(prime2)) {
+			int prime1 = primes.get(i);
+			int secret1 = secret(prime1);
+			for (int j = i; j < numPrimes; j++) {
+				int prime2 = primes.get(j);
+				int secret2 = secret(prime2);
+				if (secret1 + secret2 != secret(prime1 + prime2)) {
 					System.out.println("Secret is not additive!");
 					System.exit(0);
 				}
-			});
-			primeIterator.remove();
+			}
+
 		}
 		System.out.println("Secret is additive.");
 
@@ -56,7 +53,7 @@ public class IsSecretAdditive {
 	 * @param maxNum
 	 * @return A list of primes up to the maxNum.
 	 */
-	public static List<Integer> getPrimes(int maxNum) {
+	private static List<Integer> getPrimes(int maxNum) {
 		// Size list appropriately estimating number of primes.
 		List<Integer> primeList = new ArrayList((int) (maxNum / Math.log(maxNum)));
 
@@ -86,8 +83,57 @@ public class IsSecretAdditive {
 		return primeList;
 	}
 
+	/**
+	 * 
+	 * @param maxNum
+	 * @return A list of primes up to the maxNum.
+	 */
+	private static List<Integer> getPrimes2(int maxNum) {
+
+		// We assume even numbers are not prime
+		boolean[] isPrime = new boolean[maxNum / 2 + 1];
+		for (int i = 0; i <= isPrime.length - 1; i++) {
+			isPrime[i] = true;
+		}
+
+		// Mark all multiples as non-primes
+		// Eliminate evens
+		for (int i = 1; (i * 2) + 1 <= Math.sqrt(maxNum); i++) {
+			if (isPrime[i]) {
+				int n = i * 2 + 1;
+				for (int j = n; n * j <= maxNum; j += 2) {
+					int index = ((j * n) - 1) / 2;
+					isPrime[index] = false;
+				}
+			}
+		}
+
+		// Size list appropriately by estimating the number of primes below the
+		// given number
+		List<Integer> primeList = new ArrayList((int) (maxNum / Math.log(maxNum)));
+		
+		// Because we eliminated all evens we treat 2 as a special case and add
+		// it manually
+		primeList.add(2);
+		for (int i = 1; i < isPrime.length; i++) {
+			// Eliminating evens resulted in 2 and 3 being treated identically,
+			// we need to exit early if called with 2
+			if (maxNum == 2)
+				break;
+			if (isPrime[i]) {
+				primeList.add(i * 2 + 1);
+			}
+		}
+		return primeList;
+	}
+
+	/**
+	 * Test function
+	 * @param n
+	 * @return A function of n.
+	 */
 	private static int secret(int n) {
-		if (n == 4) {
+		if (n == 6) {
 			return 3;
 		} else
 			return n;
